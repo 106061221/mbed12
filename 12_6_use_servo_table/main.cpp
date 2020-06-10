7616a0e3bc413cf2a45b7b6f521aa3bd84240e1c
ee2405 <rio714tw@gmail.com>
2020-06-10 10:08:48 +0800

    origin

---
+++ 12_6_use_servo_table/main.cpp
@@ -0,0 +1,93 @@
+#include "mbed.h"
+
+
+#define CENTER_BASE 1500
+
+
+Serial pc(USBTX, USBRX);
+
+DigitalIn encoder(D10);
+
+PwmOut servo(D11);
+
+
+Timer t;
+
+Ticker encoder_ticker;
+
+
+volatile int steps;
+
+volatile int last;
+
+
+void servo_control(int speed){
+
+    if (speed > 200)       speed = 200;
+
+    else if (speed < -200) speed = -200;
+
+
+    servo=(CENTER_BASE + speed)/20000.0f;
+
+}
+
+
+void encoder_control(){
+
+    int value = encoder;
+
+    if(!last && value) steps++;
+
+    last = value;
+
+}
+
+
+
+int main() {
+
+    int i=0;
+    pc.baud(9600);
+
+
+    encoder_ticker.attach(&encoder_control, .001);
+
+
+    servo.period(.02);
+
+
+    while(1) {
+
+
+    //TODO: revise this value according to your result
+    if(i<5){
+        servo_control(-25.373);
+        
+    }else if(i<10){
+        servo_control(44.427);
+    }else{
+        servo_control(0);
+    }
+    i++;
+    if(i==10){
+        i=0;
+    }
+    steps = 0;
+
+    t.reset();
+
+    t.start();
+
+
+    wait(1);
+
+
+    float time = t.read();
+
+
+    pc.printf("%1.3f\r\n", (float)steps*6.5*3.14/32/time);
+
+    }
+
+}
